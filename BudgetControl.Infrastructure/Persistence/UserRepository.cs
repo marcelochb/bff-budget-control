15c3b78cf6efe5420a309ed13d6673dc8d5c3fc8
Marcelo Block Teixeira <marcelochb@gmail.com>
2024-02-04 23:59:16 -0300

    feat: Implement repository user get

--- BudgetControl.Infrastructure/Persistence/UserRepository.cs
+++ BudgetControl.Infrastructure/Persistence/UserRepository.cs
@@ -22,4 +22,9 @@ public class UserRepository : IUserRepository<User>
         var index = _users.FindIndex(u => u.Id == user.Id);
         if (index > -1) _users[index] = user;
     }
+
+    public User? Get(string id)
+    {
+        return _users.SingleOrDefault(u => u.Id.Value.ToString() == id);
+    }
 }
