4c523fad35ead127cd27746b6b2236e7f182a438
Marcelo Block Teixeira <marcelochb@gmail.com>
2024-02-13 14:18:52 -0300

    feat: Repository group interface

---
+++ BudgetControl.Interfaces/Persistence/Groups/IGroupRepository.cs
@@ -0,0 +1,23 @@
+namespace BudgetControl.Interfaces.Persistence.Groups;
+
+public interface IGroupRepository<T>
+{
+    T? GetById(string ledgerId,
+               string categoryId,
+               string id);
+    bool GetByName(string ledgerId,
+                   string categoryId,
+                   string name);
+    void Add(string ledgerId,
+             string categoryId,
+             T group);
+    void Update(string ledgerId,
+                string categoryId,
+                T group);
+    void Remove(string ledgerId,
+                string categoryId,
+                string id);
+    List<T> GetList(string ledgerId,
+                    string categoryId,
+                    string userId);
+}
