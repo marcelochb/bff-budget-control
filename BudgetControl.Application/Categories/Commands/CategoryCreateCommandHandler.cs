7ecac4245627c6f8dae7d87dacbfdf4a7fe944c1
Marcelo Block Teixeira <marcelochb@gmail.com>
2024-03-01 22:56:44 -0300

    feat: removendo o categoryRepository

--- BudgetControl.Application/Categories/Commands/CategoryCreateCommandHandler.cs
+++ BudgetControl.Application/Categories/Commands/CategoryCreateCommandHandler.cs
@@ -11,29 +11,26 @@ namespace BudgetControl.Application.Categories.Commands;
 
 public class CategoryCreateCommandHandler : IRequestHandler<CategoryCreateCommand, ErrorOr<CategoryResult>>
 {
-    private readonly ICategoryRepository<LedgerCategory> _categoryRepository;
     private readonly ILedgerRepository<Ledger> _ledgerRepository;
 
     public CategoryCreateCommandHandler(ICategoryRepository<LedgerCategory> categoryRepository, ILedgerRepository<Ledger> ledgerRepository)
     {
-        _categoryRepository = categoryRepository;
         _ledgerRepository = ledgerRepository;
     }
 
     public async Task<ErrorOr<CategoryResult>> Handle(CategoryCreateCommand command, CancellationToken cancellationToken)
     {
-        await Task.CompletedTask;
         if (command.LedgerId is null) {
             return Errors.Ledger.NotFound;
         }
-        var ledger =  _ledgerRepository.GetById(command.LedgerId);
+        var ledger = await _ledgerRepository.GetById(command.LedgerId);
         if (ledger is null)
         {
             return Errors.Ledger.NotFound;
         }
-
         var category = LedgerCategory.Create(command.Name, command.Goal, command.Color);
-        _categoryRepository.Add(command.LedgerId, category);
+        ledger.AddCategory(category);
+        await _ledgerRepository.Update(ledger);
         return new CategoryResult(category.Name, category.Goal, category.Color);
     }
 }
