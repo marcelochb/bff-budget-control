a96d0b366ecf181a45c7bc1275ae754a674d5491
Marcelo Block Teixeira <marcelochb@gmail.com>
2024-03-24 19:51:13 -0300

    feat: Mapping query response on Contract layer

--- BudgetControl.Contracts/Ledgers/Response/LedgerResponse.cs
+++ BudgetControl.Contracts/Ledgers/Response/LedgerResponse.cs
@@ -1,7 +1,8 @@
-using BudgetControl.Contracts.Authentication.Response;
 
 namespace BudgetControl.Contracts.Ledgers.Response;
 
 public record LedgerResponse(Guid Id,
                              string Name,
-                             string Type);
+                             string Type,
+                             Guid UserId,
+                             List<LedgerCategoryResponse> Categories);
